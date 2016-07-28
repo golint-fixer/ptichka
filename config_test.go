4cf0d7f3cc8f62bd5dc9233b75a31e9d90df25e5
Danil Kutkevich <danil@kutkevich.org>
2016-07-29 02:31:18 +0300

    test: tiny update

--- config_test.go
+++ config_test.go
@@ -12,67 +12,81 @@ func TestLoadConfig(t *testing.T) {
 
 	config := configs.Accounts[0]
 
-	if config.CacheFile != ".ptichka1.json" {
-		t.Errorf("CacheFile == %v, want %v", config.CacheFile, ".ptichka1.json")
+	var wont string
+	var wontInt int
+
+	wont = ".ptichka1.json"
+	if config.CacheFile != wont {
+		t.Errorf("CacheFile == %v, want %v", config.CacheFile, wont)
 	}
-	if config.Label != "[twitter account 1] " {
-		t.Errorf("Label == %v, want %v", config.Label, "[twitter account 1] ")
+
+	wont = "[twitter account 1] "
+	if config.Label != wont {
+		t.Errorf("Label == %v, want %v", config.Label, wont)
 	}
-	if config.Mail.From.Address != "noreply@example.com" {
-		t.Errorf(
-			"Mail.From.Address == %v, want %v",
-			config.Mail.From.Address,
-			"noreply@example.com")
+
+	wont = "noreply@example.com"
+	if config.Mail.From.Address != wont {
+		t.Errorf("Mail.From.Address == %v, want %v", config.Mail.From.Address, wont)
 	}
-	if config.Mail.To.Address != "your.mail+twitter1@example.org" {
-		t.Errorf(
-			"Mail.To.Address == %v, want %v",
-			config.Mail.To.Address,
-			"your.mail+twitter1@example.org")
+
+	wont = "your.mail+twitter1@example.org"
+	if config.Mail.To.Address != wont {
+		t.Errorf("Mail.To.Address == %v, want %v", config.Mail.To.Address, wont)
 	}
-	if config.Mail.SMTP.Address != "mail.example.com" {
-		t.Errorf(
-			"Mail.SMTP.Address == %v, want %v",
-			config.Mail.SMTP.Address,
-			"mail.example.com")
+
+	wont = "mail.example.com"
+	if config.Mail.SMTP.Address != wont {
+		t.Errorf("Mail.SMTP.Address == %v, want %v", config.Mail.SMTP.Address, wont)
 	}
-	if config.Mail.SMTP.Password != "your_password" {
-		t.Errorf(
-			"Mail.SMTP.Password == %v, want %v",
-			config.Mail.SMTP.Password,
-			"your_password")
+
+	wont = "your_password"
+	if config.Mail.SMTP.Password != wont {
+		t.Errorf("Mail.SMTP.Password == %v, want %v", config.Mail.SMTP.Password, wont)
 	}
-	if config.Mail.SMTP.Port != 25 {
-		t.Errorf("Mail.SMTP.Port == %v, want %v", config.Mail.SMTP.Port, 25)
+
+	wontInt = 25
+	if config.Mail.SMTP.Port != wontInt {
+		t.Errorf("Mail.SMTP.Port == %v, want %v", config.Mail.SMTP.Port, wontInt)
 	}
-	if config.Mail.SMTP.UserName != "your.mail@example.org" {
+
+	wont = "your.mail@example.org"
+	if config.Mail.SMTP.UserName != wont {
 		t.Errorf(
 			"Mail.SMTP.UserName == %v, want %v",
 			config.Mail.SMTP.UserName,
-			"your.mail@example.org")
+			wont)
 	}
-	if config.Twitter.ConsumerKey != "your-consumer-key1" {
+
+	wont = "your-consumer-key1"
+	if config.Twitter.ConsumerKey != wont {
 		t.Errorf(
 			"Twitter.ConsumerKey == %v, want %v",
 			config.Twitter.ConsumerKey,
-			"your-consumer-key1")
+			wont)
 	}
-	if config.Twitter.ConsumerSecret != "your-consumer-secret1" {
+
+	wont = "your-consumer-secret1"
+	if config.Twitter.ConsumerSecret != wont {
 		t.Errorf(
 			"Twitter.ConsumerSecret == %v, want %v",
 			config.Twitter.ConsumerSecret,
-			"your-consumer-secret1")
+			wont)
 	}
-	if config.Twitter.AccessToken != "your-access-token1" {
+
+	wont = "your-access-token1"
+	if config.Twitter.AccessToken != wont {
 		t.Errorf(
 			"Twitter.AccessToken == %v, want %v",
 			config.Twitter.AccessToken,
-			"your-access-token1")
+			wont)
 	}
-	if config.Twitter.AccessTokenSecret != "your-access-token-secret1" {
+
+	wont = "your-access-token-secret1"
+	if config.Twitter.AccessTokenSecret != wont {
 		t.Errorf(
 			"Twitter.AccessTokenSecret == %v, want %v",
 			config.Twitter.AccessTokenSecret,
-			"your-access-token-secret1")
+			wont)
 	}
 }
